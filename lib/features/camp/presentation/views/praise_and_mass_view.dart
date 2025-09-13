cd4211cc362bf311e3e7af51abcf489fa24040e0
marievasheaa <marievaheaa170@gmail>
2025-09-13 17:57:58 +0300

    finish praise_and_mass feature

---
+++ lib/features/camp/presentation/views/praise_and_mass_view.dart
@@ -0,0 +1,25 @@
+import 'package:efraho/features/camp/presentation/widgets/praise_and_mass_body.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/Constants.dart';
+import '../../../../core/utils/back_icon.dart';
+
+class PraiseAndMassView extends StatelessWidget {
+  const PraiseAndMassView({super.key});
+static const routeName='Praise_Mass';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackIcon(color: Colors.white,),
+        elevation: 0,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              color: gradientAppBarColor
+          ),
+        ),
+      ),
+      body: SafeArea(child:PraiseAndMassBody()),
+    );
+  }
+}
